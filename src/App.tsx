dd4d7f0add90e07bb98962296ad389baaa4cd25e
Walter Blacke <walter.blacke@vegabyte.studio>
2023-12-24 22:08:47 +0000

    how is this working wtf

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import type { Component } from "solid-js";
+import type { Component, ParentComponent } from "solid-js";
 import { Router, Route, useNavigate } from "@solidjs/router";
+
+const RootLayout : ParentComponent = (props) => {
+  return (
+    <>
+    Root
+    {props.children}
+    </>
+  )
+}
 
 const FooRoute: Component = () => {
   const navigate = useNavigate();
@@ -15,19 +24,17 @@ const FooRoute: Component = () => {
 const RootRoute = () => {
   const navigate = useNavigate();
   
-  
   return (
     <div>
       Root
       <button onClick={() => navigate("/foo")}>to /foo</button>
     </div>
-  );
-};
+  );};
 
 const App: Component = () => {
   return (
     <>
-      <Router base={import.meta.env.BASE_URL}>
+      <Router base={import.meta.env.BASE_URL} root={RootLayout}>
         <Route path={"/"} component={RootRoute} />
         <Route path={"/foo"} component={FooRoute} />
       </Router>
